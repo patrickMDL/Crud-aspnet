44a815417448c14d994ed6bdec91a37e3f1c7367
Patrick Medeiros De Luca <patrick_mdl@edu.univali.br>
2020-01-31 14:52:26 -0300

    Edit is not saving, but the rest works fine.

--- Crud-test-week/Crud-test-week/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Crud_test_week.MvcApplication" Language="C#" %>
---
+++ CrudExemple/CrudExemple/Models/Users.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace CrudExemple.Models
+{
+    public class Users : BaseEntity
+    {
+        [Key]
+        public long Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+        public string Address { get; set; }
+        [Required]
+        public string Phone { get; set; }
+    }
+}
